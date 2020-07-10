65c3c3d06f3616f55270de6f133e4576c570cf2c
Ahmed El-Khuffash <ahmed@el-khuffash.com>
2020-07-10 14:20:23 -0400

    Initial Commit

---
+++ WizardKit/WizardKit.h
@@ -0,0 +1,19 @@
+//
+//  WizardKit.h
+//  WizardKit
+//
+//  Created by Ahmed El-Khuffash on 2020-07-10.
+//  Copyright © 2020 Ahmed El-Khuffash. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WizardKit.
+FOUNDATION_EXPORT double WizardKitVersionNumber;
+
+//! Project version string for WizardKit.
+FOUNDATION_EXPORT const unsigned char WizardKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WizardKit/PublicHeader.h>
+
+
